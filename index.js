7126340ec11df8b7651bbbe1f28e0c8d6aae8c9d
chithiendo <baconhunt3pun0suy@hotmail.com>
2024-05-08 21:50:55 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
-function findMin(nums) {
-  let left = 0;
-  let right = nums.length - 1;
-  while (left < right) {
-    const mid = Math.floor((left + right) / 2);
-    if (nums[mid] > nums[right]) left = mid + 1;
-    else right = mid;
+const countingSort = (arr) => {
+  const min = Math.min(...arr);
+  const max = Math.max(...arr);
+  const count = Array(max - min + 1).fill(0);
+  for (let num of arr) {
+    count[num - min]++;
   }
-  return nums[left];
-}
+  let sortedIndex = 0;
+  for (let i = min; i <= max; i++) {
+    while (count[i - min] > 0) {
+      arr[sortedIndex++] = i;
+      count[i - min]--;
+    }
+  }
+  return arr;
+};
