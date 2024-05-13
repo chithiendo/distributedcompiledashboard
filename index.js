90ce554021ebbf824a7462ff344cc0c698095f39
chithiendo <baconhunt3pun0suy@hotmail.com>
2024-05-14 01:37:53 +0700

    feat project

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
-const numbers = [1, 5, 2, 8];
-const max = Math.max(...numbers);
-console.log(max); // 8
+function isAnagram(s, t) {
+  if (s.length !== t.length) return false;
+  const count = Array(26).fill(0);
+  for (let i = 0; i < s.length; i++) {
+    count[s.charCodeAt(i) - 97]++;
+    count[t.charCodeAt(i) - 97]--;
+  }
+  return count.every((c) => c === 0);
+}
